13ee26f67e1951a97b74134a1ca06cb93aecb73c
Eloi Alonso quinonero <ealonso-@cbr8s3.42barcelona.com>
2022-01-19 19:53:05 +0100

    strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -3,22 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ealonso- <ealonso-@student.42barcelona.    +#+  +:+       +#+        */
+/*   By: ealonso- <ealonso-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/12 15:03:18 by ealonso-          #+#    #+#             */
-/*   Updated: 2022/01/12 16:10:25 by ealonso-         ###   ########.fr       */
+/*   Updated: 2022/01/19 19:52:16 by ealonso-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcat(char *dest, char *src, size_t x)
+size_t	ft_strlcat(char *dest, char *src, size_t size)
 {
-	size_t	i;
-	size_t	j;
+	size_t	lendest;
+	size_t	lensrc;
+	size_t	lenboth;
 
-	i = ft_strlen(dest);
-	ft_strlcpy(dest + i, src, x);
-	j = ft_strlen(dest);
-	return (j);
+	lendest = ft_strlen(dest);
+	lensrc = ft_strlen(src);
+	if (lendest < size)
+		lenboth = lendest + lensrc;
+	else if (size == 0)
+		return (lensrc);
+	else
+		return (size + lensrc);
+	ft_strlcpy(dest + lendest, src, size - lendest);
+	return (lenboth);
 }

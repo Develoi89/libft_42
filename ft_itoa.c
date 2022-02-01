413cc2c55807b247085cdb90ae8386f50ade1a6b
Eloi Alonso quinonero <ealonso-@cbr8s2.42barcelona.com>
2022-02-01 15:09:29 +0100

    incidencia

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: ealonso- <ealonso-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/31 17:41:30 by ealonso-          #+#    #+#             */
-/*   Updated: 2022/01/31 19:31:44 by ealonso-         ###   ########.fr       */
+/*   Updated: 2022/02/01 15:08:53 by ealonso-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,16 +31,8 @@ int	ftt_intlen(int x)
 
 void	ftt_putstr(char *res, int x)
 {
-	int	count;
-	int	aux;
-
-	aux = x;
-	count = ftt_intlen(x) - 1;
-	while (aux / 10 != 0)
-	{
-		aux = (aux % x) + '0';
-		res[count] = aux;
-	}
+	if (x != 0)
+		ftt_putstr(res, x / 10);
 	res[ftt_intlen(x)] = '\0';
 	printf("prueba%s\n", res);
 }

7fb8c9b08fc96d1e4d756bc8d2250db6846870ae
Eloi Alonso quinonero <ealonso-@cbr8s2.42barcelona.com>
2022-02-07 18:23:13 +0100

    segundo final

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,21 +6,28 @@
 /*   By: ealonso- <ealonso-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/24 19:40:07 by ealonso-          #+#    #+#             */
-/*   Updated: 2022/01/25 17:06:02 by ealonso-         ###   ########.fr       */
+/*   Updated: 2022/02/07 18:22:28 by ealonso-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strdup(const char *s1)
+char	*ft_strdup(const char *s)
 {
-	char	i;
 	char	*res;
+	int		i;
+	size_t	slen;
 
-	i = ft_strlen(s1);
-	res = malloc (i * sizeof(char) + 1);
+	i = 0;
+	slen = ft_strlen(s);
+	res = malloc(sizeof(char) * (slen + 1));
 	if (!res)
-		return (NULL);
-	ft_strlcpy(res, s1, i + 1);
+		return (0);
+	while (s[i])
+	{
+		res[i] = s[i];
+		i++;
+	}
+	res[i] = '\0';
 	return (res);
 }

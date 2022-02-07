1e844b798bc2dc34a68eeaf6227d41fe87d97dd3
Eloi Alonso quinonero <ealonso-@cbr8s2.42barcelona.com>
2022-02-07 16:20:42 +0100

    finish

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: ealonso- <ealonso-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/03 19:20:35 by ealonso-          #+#    #+#             */
-/*   Updated: 2022/02/03 19:44:15 by ealonso-         ###   ########.fr       */
+/*   Updated: 2022/02/07 16:08:55 by ealonso-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,19 @@
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*newlist;
-	t_list	*newtile;
+	t_list	*newpart;
 
-	while (lst)
+	newlist = NULL;
+	while (lst && f)
 	{
-		newtile = ft_lstnew(f(lst->content));
+		newpart = ft_lstnew(f(lst->content));
+		if (!newpart)
+		{
+			ft_lstclear(&newlist, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&newlist, newpart);
 		lst = lst->next;
 	}
+	return (newlist);
 }

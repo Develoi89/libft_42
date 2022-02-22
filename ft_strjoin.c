14274a10d691782ab400265a7e1689bf1fd189f0
Eloi Alonso quinonero <ealonso-@cbr8s2.42barcelona.com>
2022-02-22 17:53:19 +0100

    actualization

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -12,20 +12,30 @@
 
 #include "libft.h"
 
-char	*ft_strjoin(char const *s1, char const *s2)
+char	*ft_strjoin(char *s1, char *s2)
 {
-	size_t	len1;
-	size_t	len2;
-	char	*join;
+	size_t	i;
+	size_t	j;
+	char	*str;
 
+	if (!s1)
+	{
+		s1 = (char *)malloc(1 * sizeof(char));
+		s1[0] = '\0';
+	}
 	if (!s1 || !s2)
 		return (NULL);
-	len1 = ft_strlen(s1);
-	len2 = ft_strlen(s2);
-	join = malloc(len1 + len2 + 1 * sizeof(char));
-	if (!join)
+	str = malloc(sizeof(char) * ((ft_strlen(s1) + ft_strlen(s2)) + 1));
+	if (str == NULL)
 		return (NULL);
-	ft_memmove(join, s1, len1);
-	ft_memmove(join + len1, s2, len2 + 1);
-	return (join);
+	i = -1;
+	j = 0;
+	if (s1)
+		while (s1[++i] != '\0')
+			str[i] = s1[i];
+	while (s2[j] != '\0')
+		str[i++] = s2[j++];
+	str[ft_strlen(s1) + ft_strlen(s2)] = '\0';
+	free(s1);
+	return (str);
 }

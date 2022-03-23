fea04bd68eaddf867ac27609ab1dc08818901397
Eloi Alonso quinonero <ealonso-@cbr7s5.42barcelona.com>
2022-03-23 15:45:20 +0100

    preprintf

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,36 +6,36 @@
 /*   By: ealonso- <ealonso-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/25 18:20:37 by ealonso-          #+#    #+#             */
-/*   Updated: 2022/01/25 19:24:43 by ealonso-         ###   ########.fr       */
+/*   Updated: 2022/02/22 18:07:53 by ealonso-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strjoin(char *s1, char *s2)
+char	*ft_strjoin(char const *s1, char const *s2)
 {
-	size_t	i;
-	size_t	j;
-	char	*str;
+	char	*new_s;
+	size_t	s1_len;
+	size_t	s2_len;
+	int		aux1;
+	int		aux2;
 
-	if (!s1)
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
+	aux1 = 0;
+	aux2 = 0;
+	new_s = malloc(s1_len + s2_len + 1);
+	if (!s1 || !s2 || !new_s)
+		return (NULL);
+	while (s1[aux1])
 	{
-		s1 = (char *)malloc(1 * sizeof(char));
-		s1[0] = '\0';
+		new_s[aux1] = s1[aux1];
+		aux1++;
 	}
-	if (!s1 || !s2)
-		return (NULL);
-	str = malloc(sizeof(char) * ((ft_strlen(s1) + ft_strlen(s2)) + 1));
-	if (str == NULL)
-		return (NULL);
-	i = -1;
-	j = 0;
-	if (s1)
-		while (s1[++i] != '\0')
-			str[i] = s1[i];
-	while (s2[j] != '\0')
-		str[i++] = s2[j++];
-	str[ft_strlen(s1) + ft_strlen(s2)] = '\0';
-	free(s1);
-	return (str);
+	while (s2[aux2])
+	{
+		new_s[aux1++] = s2[aux2++];
+	}
+	new_s[aux1] = '\0';
+	return (new_s);
 }

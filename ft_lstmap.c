ec18ddd69e2bb89bb1d38e53a18a5ef41db39efc
Eloy Alonso <develoi89@Jarvis.local>
2022-07-07 19:47:25 +0200

    patching lstmap, left just few mem errors

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstmap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ealonso- <ealonso-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: develoi89 <develoi89@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/03 19:20:35 by ealonso-          #+#    #+#             */
-/*   Updated: 2022/02/07 16:08:55 by ealonso-         ###   ########.fr       */
+/*   Updated: 2022/07/07 19:45:38 by develoi89        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,22 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	t_list	*newlist;
 	t_list	*newpart;
 
-	newlist = NULL;
-	while (lst && f)
+	if (!f || !del)
+		return (NULL);
+	while (lst->next != NULL)
 	{
-		newpart = ft_lstnew(f(lst->content));
+		newpart = ft_lstnew((*f)(lst->content));
 		if (!newpart)
 		{
-			ft_lstclear(&newlist, del);
+			while (newlist->next != NULL)
+			{
+				newpart = newlist->next;
+				(*del)(newpart->content);
+				free (newpart);
+				newlist = newpart;
+			}
+			(*del)(newlist->content);
+			free (newlist);
 			return (NULL);
 		}
 		ft_lstadd_back(&newlist, newpart);

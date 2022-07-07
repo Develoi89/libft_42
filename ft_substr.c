ce8b90e79711a1873132e5fd8e8b074029783499
Eloy Alonso <develoi89@Jarvis.local>
2022-07-07 17:36:36 +0200

    ft_substr patched :)))

--- ft_substr.c
+++ ft_substr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_substr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ealonso- <ealonso-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: develoi89 <develoi89@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/25 15:20:16 by ealonso-          #+#    #+#             */
-/*   Updated: 2022/01/26 19:37:38 by ealonso-         ###   ########.fr       */
+/*   Updated: 2022/07/07 17:31:09 by develoi89        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,17 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*new;
 	size_t	i;
+	size_t	j;
 
 	if (!s)
 		return (NULL);
-	if (start >= ft_strlen(s))
+	if (start >= ft_strlen(s) || len == 0)
 		return (ft_strdup(""));
-	if (ft_strlen(s) > len)
+	j = ft_strlen(s) - (start);
+	if (j > len)
 		i = len;
 	else
-		i = ft_strlen(s);
+		i = j;
 	new = malloc(i + 1 * sizeof(char));
 	if (!new)
 		return (NULL);
